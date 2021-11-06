9be99a7e57a01f6a8844aa9491e060ff3ef28ecd
BaselAllam <baseljahen@gmail.com>
2021-11-06 15:04:42 +0200

    commit

---
+++ itgate/lib/theme/shared_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+
+
+Color blackColor = Color(0xff08080a);
+
+Color primaryColor = Color(0xff343987);
+
+Color secondaryColor = Color(0xffe38318);
---
+++ itgate/lib/theme/shared_font_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:itgate/theme/shared_color.dart';
+
+
+
+TextStyle primaryFontStyle = TextStyle(color: primaryColor, fontSize: 20.0, fontWeight: FontWeight.bold);
+
+TextStyle primaryBlackFontStyle = TextStyle(color: blackColor, fontSize: 20.0, fontWeight: FontWeight.bold);
+
+TextStyle secondaryTextStyle = TextStyle(color: secondaryColor, fontSize: 18.0, fontWeight: FontWeight.bold);
